91c3976d28105634aaad1e0afcf1b9e88f0d3a15
Sergei Kuleshov <kilinserg89@mail.ru>
2019-04-22 15:35:50 +0300

    XSS protection function htmlspecialchars

--- templates/index.php
+++ templates/index.php
@@ -30,12 +30,12 @@
                         <img src="" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?=$ads[$key]['category']?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ads[$key]['title']?></a></h3>
+                        <span class="lot__category"><?=htmlspecialchars($ads[$key]['category']);?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=htmlspecialchars($ads[$key]['title']);?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?=price_formatting($ads[$key]['price'])?></span>
+                                <span class="lot__cost"><?=price_formatting($ads[$key]['price']);?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
