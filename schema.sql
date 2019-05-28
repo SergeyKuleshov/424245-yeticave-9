b61ad1f6b8bb141bc3ad048eabf8fb97e89b92c2
Sergei Kuleshov <kilinserg89@mail.ru>
2019-05-28 10:57:55 +0300

    change schema.sql

--- schema.sql
+++ schema.sql
@@ -1,67 +1,183 @@
-CREATE TABLE bet (
-  id int(11) NOT NULL,
-  date_add timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  current_bet int(128) NOT NULL,
-  lot_id int(11) NOT NULL,
-  user_current_winner_id int(11) NOT NULL
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: localhost
+-- Время создания: Май 28 2019 г., 10:53
+-- Версия сервера: 5.7.26-0ubuntu0.16.04.1
+-- Версия PHP: 7.3.5-1+ubuntu16.04.1+deb.sury.org+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данных: `yeticave`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `bet`
+--
+
+CREATE TABLE `bet` (
+  `id` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `current_bet` int(128) NOT NULL,
+  `lot_id` int(11) NOT NULL,
+  `user_current_winner_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE categories (
-  id int(11) NOT NULL,
-  name varchar(128) NOT NULL,
-  char_code varchar(64) NOT NULL
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `categories`
+--
+
+CREATE TABLE `categories` (
+  `id` int(11) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `char_code` varchar(64) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-INSERT INTO categories (id, name, char_code) VALUES
+--
+-- Дамп данных таблицы `categories`
+--
+
+INSERT INTO `categories` (`id`, `name`, `char_code`) VALUES
 (1, 'Сноуборд', 'Snowboard');
 
-CREATE TABLE lot (
-  id int(11) NOT NULL,
-  date_create timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  name varchar(128) NOT NULL,
-  description text NOT NULL,
-  start_price int(128) NOT NULL,
-  picture_path varchar(128) NOT NULL,
-  date_end timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  bet_step int(128) NOT NULL,
-  category_id int(11) NOT NULL,
-  user_author_id int(11) NOT NULL,
-  user_winner_id int(11) NOT NULL
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `lot`
+--
+
+CREATE TABLE `lot` (
+  `id` int(11) NOT NULL,
+  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `name` varchar(128) NOT NULL,
+  `description` text NOT NULL,
+  `start_price` int(128) NOT NULL,
+  `picture_path` varchar(128) NOT NULL,
+  `date_end` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `bet_step` int(128) NOT NULL,
+  `category_id` int(11) NOT NULL,
+  `user_author_id` int(11) NOT NULL,
+  `user_winner` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+-- --------------------------------------------------------
 
-CREATE TABLE users (
-  id int(11) NOT NULL,
-  date_add timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  email varchar(128) NOT NULL,
-  name varchar(128) NOT NULL,
-  password varchar(128) NOT NULL,
-  avatar_path varchar(128) NOT NULL,
-  contact_info varchar(128) NOT NULL,
-  created_lot_id int(11) NOT NULL,
-  user_bet_id int(11) NOT NULL
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `date_add` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `email` varchar(128) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `avatar_path` varchar(128) NOT NULL,
+  `contact_info` varchar(128) NOT NULL,
+  `created_lot_id` int(11) NOT NULL,
+  `user_bet_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-ALTER TABLE bet
-  ADD PRIMARY KEY (id),
-  ADD KEY lot_id (lot_id)
+--
+-- Индексы сохранённых таблиц
+--
 
-ALTER TABLE categories
-  ADD PRIMARY KEY (id);
+--
+-- Индексы таблицы `bet`
+--
+ALTER TABLE `bet`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `lot_id` (`lot_id`),
+  ADD KEY `lot_id_2` (`lot_id`),
+  ADD KEY `lot_id_3` (`lot_id`),
+  ADD KEY `user_current_winner_id` (`user_current_winner_id`);
 
-ALTER TABLE lot
-  ADD PRIMARY KEY (id);
+--
+-- Индексы таблицы `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`id`);
 
-ALTER TABLE users
-  ADD PRIMARY KEY (id),
-  ADD UNIQUE KEY email (email),
-  ADD UNIQUE KEY name (name);
+--
+-- Индексы таблицы `lot`
+--
+ALTER TABLE `lot`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `category_id` (`category_id`),
+  ADD KEY `user_author_id` (`user_author_id`);
 
-ALTER TABLE bet
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE categories
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-ALTER TABLE lot
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-ALTER TABLE users
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `name` (`name`),
+  ADD KEY `created_lot_id` (`created_lot_id`),
+  ADD KEY `user_bet_id` (`user_bet_id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `bet`
+--
+ALTER TABLE `bet`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `categories`
+--
+ALTER TABLE `categories`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `lot`
+--
+ALTER TABLE `lot`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `bet`
+--
+ALTER TABLE `bet`
+  ADD CONSTRAINT `bet_ibfk_1` FOREIGN KEY (`lot_id`) REFERENCES `lot` (`id`),
+  ADD CONSTRAINT `bet_ibfk_2` FOREIGN KEY (`user_current_winner_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `lot`
+--
+ALTER TABLE `lot`
+  ADD CONSTRAINT `lot_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `lot_ibfk_2` FOREIGN KEY (`user_author_id`) REFERENCES `users` (`id`);
+
+--
+-- Ограничения внешнего ключа таблицы `users`
+--
+ALTER TABLE `users`
+  ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`user_bet_id`) REFERENCES `bet` (`id`),
+  ADD CONSTRAINT `users_ibfk_2` FOREIGN KEY (`created_lot_id`) REFERENCES `lot` (`id`);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
